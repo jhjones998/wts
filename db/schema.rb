64c06d20c67fb5f97b1e8022b841dd7a389f9333
James Jones <james@riptideio.com>
2016-11-06 01:35:22 -0500

    Fix welcome index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106045531) do
+ActiveRecord::Schema.define(version: 20161106055212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
