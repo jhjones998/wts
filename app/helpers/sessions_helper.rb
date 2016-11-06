6bbd1900a1be2e11577c0126cb28e0754848dd7e
James Jones <james@riptideio.com>
2016-11-05 21:08:26 -0500

    Change session to store id only

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
   # Logs in the given user.
   def log_in(user)
-    session[:user] = user
+    session[:user_id] = user.id
   end
   def check_admin
-    session.key?(:user) && session[:user]['admin']
+    session.key?(:user_id) && User.find(session[:user_id]).admin
   end
 end
