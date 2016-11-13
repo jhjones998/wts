1959c14b9a53aaf564c6d45bf0c39cba67ce9f8c
James Jones <james@riptideio.com>
2016-11-12 21:49:21 -0600

    WIP - main page view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,8 +8,36 @@ class WelcomeController < ApplicationController
 
   def researching(tech_id, val)
     tech_instance = TechInstance.find(tech_id)
-    if @current_user.can_research?(tech_instance)
+    if @current_user.owns_tech_instance?(tech_instance)
       tech_instance.update(researching: val)
+    end
+  end
+
+  def split_artifact(country_id, level)
+    country = Country.find(country_id)
+    if @current_user.owns_country?(country)
+      if 2 <= level <= 6
+        Artifact.transaction do
+          if country.artifact["l#{level}"] > 0
+            country.artifact.decrement!("l#{level}")
+            country.artifact.increment!("l#{level - 1}", by = 2)
+          end
+        end
+      end
+    end
+  end
+
+  def combine_artifacts(country_id, level)
+    country = Country.find(country_id)
+    if @current_user.owns_country?(country)
+      if 1 <= level <= 5
+        Artifact.transaction do
+          if country.artifact["l#{level - 1}"] > 0
+            country.artifact.increment!("l#{level + 1}")
+            country.artifact.decrement!("l#{level}", by = 2)
+          end
+        end
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,27 @@
 class User < ApplicationRecord
   has_secure_password
   def has_access_to_tech?(country)
-    if user.admin
+    if self.admin
       return true
     end
     self.accesses.exists?(country: country)
   end
   def has_access_to_artifacts?(country)
-    if user.admin
+    if self.admin
       return true
     end
     self.accesses.exists?(country: country, artifacts: true)
   end
-  def can_research?(tech_instance)
+  def owns_tech_instance?(tech_instance)
+    if self.admin
+      return true
+    end
     tech_instance.country.user.id == self.id
+  end
+  def owns_country?(country)
+    if self.admin
+      return true
+    end
+    country.user_id == self.id
   end
 end
